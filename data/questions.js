cab25c55da6705aed50be89ddd76bbe9ea2bd3ec
NextGenWebStudios <reachavigoyal@gmail.com>
2026-04-02 16:49:05 +0530

    Create questions.js

---
+++ data/questions.js
@@ -0,0 +1,23 @@
+const physicsQuestions=[
+{question:"v = u + at is equation of?", options:["Kinematics","Dynamics","Optics"], answer:"Kinematics"},
+{question:"Acceleration = ?", options:["v-u","v-u/t","s/t"], answer:"v-u/t"},
+{question:"Projectile motion horizontal formula?", options:["s = ut","R = u² sin2θ / g","v = u + at"], answer:"R = u² sin2θ / g"},
+{question:"Max height formula?", options:["H = u² sin²θ /2g","H = ut + 1/2 at²","v² = u² + 2as"], answer:"H = u² sin²θ /2g"},
+{question:"Displacement in uniformly accelerated motion?", options:["s=ut + 1/2 at²","s = vt","s = u+at"], answer:"s=ut + 1/2 at²"}
+];
+
+const chemistryQuestions=[
+{question:"Atomic number of Hydrogen?", options:["1","2","3"], answer:"1"},
+{question:"Formula of water?", options:["H2O","CO2","NaCl"], answer:"H2O"},
+{question:"Molar mass of NaCl?", options:["58.44 g/mol","60 g/mol","35.5 g/mol"], answer:"58.44 g/mol"},
+{question:"pH of neutral solution?", options:["7","0","14"], answer:"7"},
+{question:"Electronegativity of Oxygen?", options:["3.5","2.1","1.0"], answer:"3.5"}
+];
+
+const mathsQuestions=[
+{question:"Derivative of x²?", options:["2x","x","x²"], answer:"2x"},
+{question:"Integral of 1/x?", options:["ln|x|","x","1/x²"], answer:"ln|x|"},
+{question:"Derivative of sinx?", options:["cosx","-cosx","sinx"], answer:"cosx"},
+{question:"Integral of cosx?", options:["sinx","-sinx","cosx"], answer:"sinx"},
+{question:"d/dx e^x?", options:["e^x","xe^x","1"], answer:"e^x"}
+];
